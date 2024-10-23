edcb8fed3b38168c676f1caa48f2b252dd503062
Divya Advani <divya912003@gmail.com>
2024-10-23 18:13:51 +0530

    add

---
+++ JavaScript/basic js/basic.js
@@ -0,0 +1,21 @@
+// console.log("Hello World");
+
+// let a = 125
+// a=50
+// console.log(a)
+
+// {
+//     let a = "divya"
+//     console.log(a)
+// }
+
+// const a ="sakshi"
+// // const a = "divya"
+// console.log(a)
+
+var a="abc"
+{
+    var a ="divya"
+    console.log(a)
+}
+console.log(a)
---
+++ JavaScript/basic js/operator.js
@@ -0,0 +1,40 @@
+// let a = 10
+// let b = 5
+// let c = a + b
+// let d = a - b
+// let e = a * b
+// let f = a / b
+// let s = c + d
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(s)
+
+// let a = 5
+// let b = 10
+// let c = a>b
+// let d = a<b
+// let e = a>=b
+// let f = a<=b
+// let g = a==b
+// let h = a!=b
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+
+
+// let age = 18
+// let message = (age>18) ? "you can vote":"you cannot vote"
+// console .log(message)
+
+let score = 85
+let marks = (score>=90) ? "A": (score>=80) ? "B": (score>=70) ? "C" : False
+
+console.log(marks)
+
+
+
